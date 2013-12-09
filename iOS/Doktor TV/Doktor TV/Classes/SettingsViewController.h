1db701d1b19560902ac6e1f3ee68c77b60fa21ea
Tobias Due Munk <tobiasdm@gmail.com>
2013-12-09 20:38:37 +0100

    iOS: Initial setup of settings

---
+++ iOS/Doktor TV/Doktor TV/Classes/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Doktor TV
+//
+//  Created by Tobias DM on 09/12/13.
+//  Copyright (c) 2013 developmunk. All rights reserved.
+//
+
+#import "JMStaticContentTableViewController.h"
+
+@interface SettingsViewController : JMStaticContentTableViewController
+
+@end
---
+++ iOS/Doktor TV/Doktor TV/Classes/SettingsViewController.m
@@ -0,0 +1,43 @@
+//
+//  SettingsViewController.m
+//  Doktor TV
+//
+//  Created by Tobias DM on 09/12/13.
+//  Copyright (c) 2013 developmunk. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+// TODO: Discretionary for bacgkround transfers. Increases restrictions related to power and cellular usage
+// TODO: Allow cellular usage for file download (images and videos)
+// TODO: Rebuild database
+// TODO: Clear cached files (images and videos)
+
+@implementation SettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
